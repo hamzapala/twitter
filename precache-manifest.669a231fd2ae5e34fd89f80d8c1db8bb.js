16d41706c98e8a041d70987d7341ffc5c3d4626f
HAMZA PALA <hamzapala48@gmail.com>
2022-08-15 14:59:33 +0300

    Updates

---
+++ precache-manifest.669a231fd2ae5e34fd89f80d8c1db8bb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4367f6e20478be04529",
+    "url": "/twitter/static/css/main.26f1dad1.chunk.css"
+  },
+  {
+    "revision": "c4367f6e20478be04529",
+    "url": "/twitter/static/js/main.c4367f6e.chunk.js"
+  },
+  {
+    "revision": "c80cc9b0e7921de345f5",
+    "url": "/twitter/static/js/1.c80cc9b0.chunk.js"
+  },
+  {
+    "revision": "3f89907d52536c66f970",
+    "url": "/twitter/static/js/runtime~main.3f89907d.js"
+  },
+  {
+    "revision": "ff8f3fe221e1ce8688937ea5ff31e323",
+    "url": "/twitter/static/media/tick.ff8f3fe2.png"
+  },
+  {
+    "revision": "0bdf1ca6c5a78c0bbe51782c2943e3c5",
+    "url": "/twitter/static/media/otherProfiles.0bdf1ca6.png"
+  },
+  {
+    "revision": "244dc70ad823e37e8e35d613957a3a5e",
+    "url": "/twitter/static/media/more.244dc70a.png"
+  },
+  {
+    "revision": "7b1ab58c57838ae8287e7103ed6a59d5",
+    "url": "/twitter/static/media/kapak.7b1ab58c.png"
+  },
+  {
+    "revision": "959ccfc0978637d6048330170421771f",
+    "url": "/twitter/static/media/mainProfile.959ccfc0.png"
+  },
+  {
+    "revision": "060e48f03eca53c75554396c851b1f93",
+    "url": "/twitter/static/media/map.060e48f0.png"
+  },
+  {
+    "revision": "ec3d9d7e7eac72c8f92f8866dd2f703f",
+    "url": "/twitter/static/media/balloon.ec3d9d7e.png"
+  },
+  {
+    "revision": "ad45a7c8ccc8052f01767142857b2f4b",
+    "url": "/twitter/static/media/calendar.ad45a7c8.png"
+  },
+  {
+    "revision": "537fb0ae2f20aca8f88ff0bea97c3a50",
+    "url": "/twitter/static/media/retweet.537fb0ae.png"
+  },
+  {
+    "revision": "9c425ca7ad0e9c80edba8381fdd8d425",
+    "url": "/twitter/static/media/heart.9c425ca7.png"
+  },
+  {
+    "revision": "1d6ff6673f0845bb707994328f0b1f4d",
+    "url": "/twitter/static/media/twitter.1d6ff667.png"
+  },
+  {
+    "revision": "454f44b3a58fa76db7d2a53f6f62cd78",
+    "url": "/twitter/static/media/hash.454f44b3.png"
+  },
+  {
+    "revision": "a7cd1a99175309acfc36e65b451b4a41",
+    "url": "/twitter/static/media/ring.a7cd1a99.png"
+  },
+  {
+    "revision": "9ceec343430e5c0ccf504d657010afb8",
+    "url": "/twitter/static/media/envelope.9ceec343.png"
+  },
+  {
+    "revision": "a6889952803edf290c8afbed26863a0d",
+    "url": "/twitter/static/media/bookmark.a6889952.png"
+  },
+  {
+    "revision": "eb058b955f387db059267a28afe09a9d",
+    "url": "/twitter/static/media/list.eb058b95.png"
+  },
+  {
+    "revision": "3a596d0e1dfbefe875035f4f5a556edb",
+    "url": "/twitter/static/media/profile.3a596d0e.png"
+  },
+  {
+    "revision": "859e2da26853b470074a323b1dbac99d",
+    "url": "/twitter/static/media/search.859e2da2.png"
+  },
+  {
+    "revision": "30f3967185900eb3251c4e9fbbb20431",
+    "url": "/twitter/static/media/jeff.30f39671.png"
+  },
+  {
+    "revision": "b6e3794f772edd3341375d2a04d1a177",
+    "url": "/twitter/static/media/shakira.b6e3794f.jpg"
+  },
+  {
+    "revision": "924905936dc0def6aa33bf315b43d99c",
+    "url": "/twitter/static/media/google.92490593.png"
+  },
+  {
+    "revision": "5060aa5f5e093c7a9f0a960c251501dd",
+    "url": "/twitter/index.html"
+  }
+];
